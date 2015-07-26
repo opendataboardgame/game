d5650d2f749cd1d450b528be8b4e571afac92435
Jeni Tennison <jeni@theodi.org>
2015-07-26 15:48:30 +0100

    initial tool tile design

--- deck.rb
+++ deck.rb
@@ -13,3 +13,16 @@ Squib::Deck.new(cards: 36, width: 1050, height: 675, layout: 'layout.yml') do
   save_png prefix: 'event_'
   save_sheet prefix: 'events_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
 end
+
+Squib::Deck.new(cards: 6, width: 1050, height: 675, layout: 'layout.yml') do
+  background color: '#FFFFFF'
+
+  tiles = csv file: 'tiles/tiles.csv'
+
+  svg file: tiles['data'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'ToolIcon'
+  svg file: tiles['impact'].map { |i| i == 'Social' ? "world-board/Social.svg" : "world-board/Social-light.svg" }, layout: 'ToolImpactIcon1'
+  svg file: tiles['impact'].map { |i| i == 'Environmental' ? "world-board/Environmental.svg" : "world-board/Environmental-light.svg" }, layout: 'ToolImpactIcon2'
+  svg file: tiles['impact'].map { |i| i == 'Economic' ? "world-board/Economic.svg" : "world-board/Economic-light.svg" }, layout: 'ToolImpactIcon3'
+
+  save_png prefix: 'tool_'
+end
