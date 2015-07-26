2d33734e9884655801e640252a906f51cbf1fceb
Jeni Tennison <jeni@theodi.org>
2015-07-26 14:03:31 +0100

    better event cards

--- deck.rb
+++ deck.rb
@@ -1,11 +1,15 @@
 require 'squib'
 
-Squib::Deck.new(cards: 36, height: 216, width: 336, layout: 'layout.yml') do
+Squib::Deck.new(cards: 36, width: 1050, height: 675, layout: 'layout.yml') do
   background color: '#FFFFFF'
 
   events = csv file: 'events/events.csv'
 
   text str: events['description'], layout: 'EventText'
+  svg file: events['impact'].map { |i| "world-board/#{i}.svg" }, layout: 'EventEffectIcon'
+  text str: events['score'].map { |s| s > 0 ? "+#{s}" : s }, layout: 'EventEffectText'
 
-  save_png prefix: 'event_', rotate: true
+
+  save_png prefix: 'event_'
+  save_sheet prefix: 'events_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
 end
