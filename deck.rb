d8aaf7b3cb705f9d342ba3dccb581d4f57ff23a5
Jeni Tennison <jeni@theodi.org>
2016-01-28 21:56:33 +0000

    updated gemfile

--- deck.rb
+++ deck.rb
@@ -9,7 +9,6 @@ Squib::Deck.new(cards: 27, width: 1050, height: 675, layout: 'layout.yml') do
   svg file: events['impact'].map { |i| "world-board/#{i}.svg" }, layout: 'EventEffectIcon'
   text str: events['score'].map { |s| s > 0 ? "+#{s}" : s }, layout: 'EventEffectText'
 
-
   save_png prefix: 'event_'
   save_sheet prefix: 'events_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
 end
