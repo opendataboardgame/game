b246f1438257c7791a54f542891963b0d2bf10e1
Jeni Tennison <jeni@theodi.org>
2016-01-31 21:07:58 +0000

    upgraded card generation

--- deck.rb
+++ deck.rb
@@ -1,10 +1,18 @@
 require 'squib'
 
-Squib::Deck.new(cards: 9, width: 1050, height: 675, layout: 'layout.yml') do
+# sheets are created based on printing on A4 paper
+# A4 paper = 8.3" x 11.7"
+# 825 pixels @ 300 pixels/inch = 2.75"
+# 1125 pixels @ 300 pixels/inch = 3.75"
+# 8.3" - (3.75" x 2) = 0.8"
+# 11.7" - (2.75" x 4) = 0.7"
+
+Squib::Deck.new(cards: 9, width: 1125, height: 825, layout: 'layout.yml') do
   background color: '#FFFFFF'
 
   roles = csv file: 'roles/missions.csv'
 
+  png file: "templates/pokercard.png"
   text str: roles['name'], layout: 'RoleName'
   text str: roles['role'], layout: 'RoleDescription'
   text str: "You win if", layout: 'RoleConditionTitle'
@@ -14,181 +22,81 @@ Squib::Deck.new(cards: 9, width: 1050, height: 675, layout: 'layout.yml') do
   svg file: roles['focus'].map { |i| "world-board/#{i}.svg" }, layout: 'RoleFocusIcon'
 
   save_png prefix: 'role_'
-  save_sheet prefix: 'roles_sheet_', columns: 2, rows: 4, margin: 150, gap: 75
+  save_sheet prefix: 'roles_sheet_', columns: 2, rows: 4, margin: 105
 end
 
-Squib::Deck.new(cards: 27, width: 1050, height: 675, layout: 'layout.yml') do
+Squib::Deck.new(cards: 39, width: 1125, height: 825, layout: 'layout.yml') do
   background color: '#FFFFFF'
 
   events = csv file: 'events/events.csv'
 
-  text str: events['description'], layout: 'EventText'
-  svg file: events['impact'].map { |i| "world-board/#{i}.svg" }, layout: 'EventEffectIcon'
-  text str: events['score'].map { |s| s > 0 ? "+#{s}" : s }, layout: 'EventEffectText'
+  type = {}
+  events['type'].each_with_index{ |t,i| (type[t] ||= []) << i}
+
+  png file: "templates/pokercard.png"
+
+  text range: type['role'], str: events['title'], layout: 'EventTitle'
+  text range: type['role'], str: events['description'], layout: 'EventDescription'
+
+  text range: type['legislation'], str: "New legislation", layout: 'EventTitle'
+  text range: type['legislation'], str: events['title'].map { |c| "All* #{c} data must be open" }, layout: 'EventDescription'
+  text range: type['legislation'], str: "* Except private data", layout: 'EventDisclaimer'
+
+  text range: type['impact'], str: events['title'], layout: 'EventTitle'
+  svg range: type['impact'], file: events['impact'].map { |i| "world-board/#{i || "Economic"}.svg" }, layout: 'EventEffectIcon'
+  text range: type['impact'], str: events['score'], layout: 'EventEffectText'
 
   save_png prefix: 'event_'
-  save_sheet prefix: 'events_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
+  save_sheet prefix: 'events_sheet_', columns: 2, rows: 4, margin: 105
 end
 
-Squib::Deck.new(cards: 6, width: 1050, height: 675, layout: 'layout.yml') do
+Squib::Deck.new(cards: 63, width: 1125, height: 825, layout: 'layout.yml') do
   background color: '#FFFFFF'
 
+  tools = csv file: 'tools/tools.csv'
   tiles = csv file: 'tiles/tiles.csv'
 
-  svg file: tiles['data'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'ToolIcon'
-  svg file: tiles['impact'].map { |i| i == 'Social' ? "world-board/Social.svg" : "world-board/Social-light.svg" }, layout: 'ToolImpactIcon1'
-  svg file: tiles['impact'].map { |i| i == 'Environmental' ? "world-board/Environmental.svg" : "world-board/Environmental-light.svg" }, layout: 'ToolImpactIcon2'
-  svg file: tiles['impact'].map { |i| i == 'Economic' ? "world-board/Economic.svg" : "world-board/Economic-light.svg" }, layout: 'ToolImpactIcon3'
+  type = {}
+  tools['type'].each_with_index{ |t,i| (type[t] ||= []) << i}
 
-  save_png prefix: 'tool_1_'
-  save_sheet prefix: 'tools_1_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
-end
-
-Squib::Deck.new(cards: 21, width: 1050, height: 675, layout: 'layout.yml') do
-  background color: '#FFFFFF'
-
-  data1 = []
-  data2 = []
   social = []
   environmental = []
   economic = []
-
-  tiles = csv file: 'tiles/tiles.csv'
-
-  data = tiles['data']
-
-  data.each_with_index do |d,i|
-    rest = data[i..6]
-    rest.each do |r|
-      data1.push(d)
-      data2.push(r)
-      social.push(d == 'Democracy' || d == 'Health' || r == 'Democracy' || r == 'Health')
-      environmental.push(d == 'Geography' || d == 'Weather' || r == 'Geography' || r == 'Weather')
-      economic.push(d == 'Product' || d == 'Transport' || r == 'Product' || r == 'Transport')
-    end
+  tools['type'].each_with_index do |t,i|
+    d1 = tools['data1'][i]
+    d2 = tools['data2'][i]
+    d3 = tools['data3'][i]
+    social.push(d1 == 'Democracy' || d1 == 'Health' || d2 == 'Democracy' || d2 == 'Health' || d3 == 'Democracy' || d3 == 'Health')
+    environmental.push(d1 == 'Geography' || d1 == 'Weather' || d2 == 'Geography' || d2 == 'Weather' || d3 == 'Geography' || d3 == 'Weather')
+    economic.push(d1 == 'Product' || d1 == 'Transport' || d2 == 'Product' || d2 == 'Transport' || d3 == 'Product' || d3 == 'Transport')
   end
 
-  svg file: data1.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool2Icon1'
-  svg file: data2.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool2Icon2'
+  png file: "templates/pokercard.png"
+
+  svg range: type['singleton'], file: tools['data1'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'ToolIcon'
+
+  svg range: type['pair'], file: tools['data1'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool2Icon1'
+  svg range: type['pair'], file: tools['data2'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool2Icon2'
+
+  svg range: type['straight'], file: tools['data1'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool3Icon1'
+  svg range: type['straight'], file: tools['data2'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool3Icon2'
+  svg range: type['straight'], file: tools['data3'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool3Icon3'
+
+  svg range: type['angle'], file: tools['data1'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool4Icon1'
+  svg range: type['angle'], file: tools['data2'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool4Icon2'
+  svg range: type['angle'], file: tools['data3'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool4Icon3'
+
+  svg range: type['cluster'], file: tools['data1'].map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool5Icon1'
+  svg range: type['cluster'], file: tools['data2'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool5Icon2'
+  svg range: type['cluster'], file: tools['data3'].map { |d| "tiles/#{(d || 'Transport').downcase}-closed.svg" }, layout: 'Tool5Icon3'
+
   svg file: social.map { |i| i ? "world-board/Social.svg" : "world-board/Social-light.svg" }, layout: 'ToolImpactIcon1'
   svg file: environmental.map { |i| i ? "world-board/Environmental.svg" : "world-board/Environmental-light.svg" }, layout: 'ToolImpactIcon2'
   svg file: economic.map { |i| i ? "world-board/Economic.svg" : "world-board/Economic-light.svg" }, layout: 'ToolImpactIcon3'
 
-  save_png prefix: 'tool_2_'
-  save_sheet prefix: 'tools_2_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
-end
+  text str: tools['description'], layout: 'ToolDescription'
 
-Squib::Deck.new(cards: 18, width: 1050, height: 675, layout: 'layout.yml') do
-  background color: '#FFFFFF'
-
-  data1 = []
-  data2 = []
-  data3 = []
-  social = []
-  environmental = []
-  economic = []
-
-  tiles = csv file: 'tiles/tiles.csv'
-
-  data = tiles['data']
-
-  data.each do |d1|
-    for i in 1..3
-      d2 = data[rand(6)]
-      d3 = data[rand(6)]
-      data1.push(d1)
-      data2.push(d2)
-      data3.push(d3)
-      social.push(d1 == 'Democracy' || d1 == 'Health' || d2 == 'Democracy' || d2 == 'Health' || d3 == 'Democracy' || d3 == 'Health')
-      environmental.push(d1 == 'Geography' || d1 == 'Weather' || d2 == 'Geography' || d2 == 'Weather' || d3 == 'Geography' || d3 == 'Weather')
-      economic.push(d1 == 'Product' || d1 == 'Transport' || d2 == 'Product' || d2 == 'Transport' || d3 == 'Product' || d3 == 'Transport')
-    end
-  end
-
-  svg file: data1.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool3Icon1'
-  svg file: data2.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool3Icon2'
-  svg file: data3.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool3Icon3'
-  svg file: social.map { |i| i ? "world-board/Social.svg" : "world-board/Social-light.svg" }, layout: 'ToolImpactIcon1'
-  svg file: environmental.map { |i| i ? "world-board/Environmental.svg" : "world-board/Environmental-light.svg" }, layout: 'ToolImpactIcon2'
-  svg file: economic.map { |i| i ? "world-board/Economic.svg" : "world-board/Economic-light.svg" }, layout: 'ToolImpactIcon3'
-
-  save_png prefix: 'tool_3_'
-  save_sheet prefix: 'tools_3_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
-end
-
-Squib::Deck.new(cards: 18, width: 1050, height: 675, layout: 'layout.yml') do
-  background color: '#FFFFFF'
-
-  data1 = []
-  data2 = []
-  data3 = []
-  social = []
-  environmental = []
-  economic = []
-
-  tiles = csv file: 'tiles/tiles.csv'
-
-  data = tiles['data']
-
-  data.each do |d1|
-    for i in 1..3
-      d2 = data[rand(6)]
-      d3 = data[rand(6)]
-      data1.push(d1)
-      data2.push(d2)
-      data3.push(d3)
-      social.push(d1 == 'Democracy' || d1 == 'Health' || d2 == 'Democracy' || d2 == 'Health' || d3 == 'Democracy' || d3 == 'Health')
-      environmental.push(d1 == 'Geography' || d1 == 'Weather' || d2 == 'Geography' || d2 == 'Weather' || d3 == 'Geography' || d3 == 'Weather')
-      economic.push(d1 == 'Product' || d1 == 'Transport' || d2 == 'Product' || d2 == 'Transport' || d3 == 'Product' || d3 == 'Transport')
-    end
-  end
-
-  svg file: data1.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool4Icon1'
-  svg file: data2.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool4Icon2'
-  svg file: data3.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool4Icon3'
-  svg file: social.map { |i| i ? "world-board/Social.svg" : "world-board/Social-light.svg" }, layout: 'ToolImpactIcon1'
-  svg file: environmental.map { |i| i ? "world-board/Environmental.svg" : "world-board/Environmental-light.svg" }, layout: 'ToolImpactIcon2'
-  svg file: economic.map { |i| i ? "world-board/Economic.svg" : "world-board/Economic-light.svg" }, layout: 'ToolImpactIcon3'
-
-  save_png prefix: 'tool_4_'
-  save_sheet prefix: 'tools_4_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
-  hand file: 'tools.png', range: 2..3, angle_range: (Math::PI / -4.0)..0, trim: 37.5, trim_radius: 25, fill_color: '#0000'
-end
-
-Squib::Deck.new(cards: 18, width: 1050, height: 675, layout: 'layout.yml') do
-  background color: '#FFFFFF'
-
-  data1 = []
-  data2 = []
-  data3 = []
-  social = []
-  environmental = []
-  economic = []
-
-  tiles = csv file: 'tiles/tiles.csv'
-
-  data = tiles['data']
-
-  data.each do |d1|
-    for i in 1..3
-      d2 = data[rand(6)]
-      d3 = data[rand(6)]
-      data1.push(d1)
-      data2.push(d2)
-      data3.push(d3)
-      social.push(d1 == 'Democracy' || d1 == 'Health' || d2 == 'Democracy' || d2 == 'Health' || d3 == 'Democracy' || d3 == 'Health')
-      environmental.push(d1 == 'Geography' || d1 == 'Weather' || d2 == 'Geography' || d2 == 'Weather' || d3 == 'Geography' || d3 == 'Weather')
-      economic.push(d1 == 'Product' || d1 == 'Transport' || d2 == 'Product' || d2 == 'Transport' || d3 == 'Product' || d3 == 'Transport')
-    end
-  end
-
-  svg file: data1.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool5Icon1'
-  svg file: data2.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool5Icon2'
-  svg file: data3.map { |d| "tiles/#{d.downcase}-closed.svg" }, layout: 'Tool5Icon3'
-  svg file: social.map { |i| i ? "world-board/Social.svg" : "world-board/Social-light.svg" }, layout: 'ToolImpactIcon1'
-  svg file: environmental.map { |i| i ? "world-board/Environmental.svg" : "world-board/Environmental-light.svg" }, layout: 'ToolImpactIcon2'
-  svg file: economic.map { |i| i ? "world-board/Economic.svg" : "world-board/Economic-light.svg" }, layout: 'ToolImpactIcon3'
-
-  save_png prefix: 'tool_5_'
-  save_sheet prefix: 'tools_5_sheet_', columns: 3, rows: 7, margin: 150, gap: 75
+  save_png prefix: 'tool_'
+  save_sheet prefix: 'tools_sheet_', columns: 2, rows: 4, margin: 105
+  hand file: 'tools.png', range: [59,46], angle_range: (Math::PI / -4.0)..0, trim: 38, trim_radius: 38, fill_color: '#0000'
 end
